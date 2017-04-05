8b9cc30f4ea30306bcfcce7ae428708aa895d090
Steve Hollasch <steve@hollasch.net>
2017-04-05 10:13:33 -0700

    Add comment to prop-headers.cmd

--- prop-headers.cmd
+++ prop-headers.cmd
@@ -1,1 +1,4 @@
+@REM // This build script is called during the Visual Studio build process.
+@REM // It copies all FileSystemProxy header files to the output directory for distribution.
+
 copy *.h out
